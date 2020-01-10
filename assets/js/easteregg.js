6bcb9b14c2bae80cace9677a055046e5fd524336
Ryan Bishop <rdbishop06@gmail.com>
2020-01-10 11:34:18 -0600

    easter egg for devs

---
+++ assets/js/easteregg.js
@@ -0,0 +1,7 @@
+console.log("-*--------*-")
+console.log("#DevEveryDay")
+
+console.log('Hello, fellow traveler...')
+console.log('http://127.0.0.1:8080/sourcerer.html')
+console.log('https://rdbishop19.github.io/sourcerer.html')
+console.log("-*--------*-")
